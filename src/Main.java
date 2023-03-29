980307849a41f6c4aa9c68b6858c68996d0127bd
dariaost19 <dariaost19@gmail.com>
2023-03-29 19:17:27 -0400

    the basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BasicStructure bs=new BasicStructure();
+        bs.model="A3";
+        bs.color="black";
+        bs.power="5000cc";
+    }
+}
